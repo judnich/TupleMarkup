0fcf56d78f449d41aa4945ac187e1f721f3ea249
John Judnich <electrograv@gmail.com>
2012-12-23 17:19:32 -0800

    Finished javascript support.

--- tml-js/source/tml.js
+++ tml-js/source/tml.js
@@ -2,27 +2,95 @@
 var TML = (function() {
 	var me = {};
 
-	me.parse = function(tmlString)
-	{
-		var tokens = tmlString.match(/(\[|\]|\||\w+)/g);
+	// Parses the given TML string and returns the result a a "tree" (nested Javascript lists and strings).
+	me.parse = function(tmlString) {
+		var rawString = convertIntermediateEscapeCodes(tmlString);
+		var tokens = rawString.match(/((\w|\\.)+|\[|\]|\|)/g);
 		return parseRoot(tokens);
 	}
 
-	me.toMarkupString = function(tmlTree)
-	{
+	// Converts a given Javascript nested lists/strings "tree" object into valid TML code
+	me.toMarkupString = function(tmlTree) {
 		return _treeToString(tmlTree, true);
 	}
 
-	me.toString = function(tmlTree)
-	{
+	// Converts a given Javascript nested lists/strings "tree" object into a string with each item separated by a space
+	me.toString = function(tmlTree) {
 		return _treeToString(tmlTree, false);
 	}
 
+	// Compares a candidate TML tree against a given pattern. If the pattern contains no wildcards, this
+	// simply compares the two trees and returns true if identical. Alternately, the pattern may contain 
+	// the "\?" wildcard to match any node (any list or string), or the "\*" wildcard to match any zero
+	// or more nodes up to the end of the list.
+	me.compare = function(candidate, pattern) {
+		if (pattern === "\\?")
+			return true;
+		if (typeof(candidate) === "string")
+			return (candidate === pattern);
+		if (typeof(pattern) === "string") 
+			return false;
+
+		var cLen = candidate.length, pLen = pattern.length;
+		var c;
+		for (c = 0; c < cLen; ++c) {
+			if (c >= pLen) 
+				return false;
+			if (pattern[c] === "\\*") 
+				return true;
+			if (!me.compare(candidate[c], pattern[c])) 
+				return false;
+		}
+
+		if (c < pLen) {
+			if (pattern[c] === "\\*")
+				return true;
+			else
+				return false;
+		}
+
+		return true;
+	}
+
+	// Returns the first child under node which matches the given pattern (as determined by TML.compare).
+	// Returns null if no match is found.
+	me.find = function(node, pattern) {
+		var len = node.length;
+		for (var i = 0; i < len; ++i) {
+			if (me.compare(node[i], pattern))
+				return node[i];
+		}
+		return null;
+	}
+
+	// Returns a list of all children under node which match the given pattern (as determined by TML.compare).
+	// Returns [] if no match is found.
+	me.findAll = function(node, pattern) {
+		var results = [];
+		var len = node.length;
+		for (var i = 0; i < len; ++i) {
+			if (me.compare(node[i], pattern))
+				results.push(node[i]);
+		}
+		return results;
+	}
+
+
+	var parseRoot = function(tokens) {
+		var tokLen = tokens.length;
+		if (tokLen == 0 || tokens[0] !== "[") throw "TML: Expected open bracket";
+
+		var ret = parseList(tokens, 1, false);
+		var tree = ret.result;
+
+		if (ret.index < tokLen) throw "TML: Expected end of file after root node";
+
+		return tree;
+	}
 
 	// Parses list "... ]" where the open bracket has already been read.
 	// If endAtBar is true, this will let "|" terminate the list.
-	var parseList = function(tokens, index, endAtBar)
-	{
+	var parseList = function(tokens, index, endAtBar) {
 		var i = index;
 
 		var tokLen = tokens.length;
@@ -60,32 +128,47 @@ var TML = (function() {
 			}
 			else {
 				// append word
-				if (tok !== "") list.push(tok);
+				if (tok !== "") list.push(convertFinalEscapeCodes(tok));
 			}
 		}
 
 		throw "TML: Expected close bracket";	
 	}
 
-	var parseRoot = function(tokens)
-	{
-		var tokLen = tokens.length;
-		if (tokLen == 0) return null;
+	var collapsed_OpenBracket = "\\o";
+	var collapsed_CloseBracket = "\\c";
+	var collapsed_Divider = "\\d";
 
-		var tok = tokens[0];
-		if  (tok !== "[") throw "TML: Expected open bracket";
+	var convertIntermediateEscapeCodes = function(str) {
+		var result = str.replace(/\\./g, function(code) {
+			if (code === "\\[") return collapsed_OpenBracket;
+			else if (code === "\\]") return collapsed_CloseBracket;
+			else if (code === "\\|") return collapsed_Divider;
+			return code;
+		});
+		return result;
+	}
 
-		var ret = parseList(tokens, 1, false);
-		var tree = ret.result;
-
-		if (ret.index < tokLen) throw "TML: Expected end of file after root node";
-
-		return tree;
+	var convertFinalEscapeCodes = function(str) {
+		var result = str.replace(/\\./g, function(code) {
+			if (code === collapsed_Divider) return "|";
+			else if (code === collapsed_OpenBracket) return "[";
+			else if (code === collapsed_Divider) return "]";
+			else if (code === "\\s") return " ";
+			else if (code === "\\t") return "\t";
+			else if (code === "\\n") return "\n";
+			else if (code === "\\r") return "\r";
+			else if (code === "\\\\") return "\\";
+			return code;
+		});
+		return result;
 	}
 
 
-	var _treeToString = function(tmlTree, brackets)
-	{
+	var _treeToString = function(tmlTree, brackets) {
+		if (typeof(tmlTree) === "string")
+			return tmlTree;
+
 		var len = tmlTree.length;
 		var str = "";
 
