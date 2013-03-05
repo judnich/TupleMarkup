1b440412ca8e612c21349e3f924111dae0b7372b
John Judnich <electrograv@gmail.com>
2013-03-04 21:24:02 -0800

    Added Python parser implementation (still need to add pattern matching api functions)

---
+++ tml-py/source/tml.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2013 John Judnich
+# Released as open-source under The MIT Licence.
+#
+# This module provides a parser which loads an entire TML file into memory as nested lists of strings.
+# This also provides functions to convert nested lists-of-strings objects back into TML code, pattern
+# matching functions, and searching functions (to easily locate subtrees that match a given pattern).
+
+import re
+
+splitter = re.compile(r"(\s|\[|\]|\|)")
+_inter_open, _inter_close, _inter_divider = "\\o", "\\c", "\\d"
+
+
+def _replaceMultiple(text, replaceTuples):
+	for old, new in replaceTuples:
+		text = text.replace(old, new)
+	return text
+
+
+def _convertIntermediateEscapeCodes(text):
+	replacements = [("\\[", _inter_open), ("\\]", _inter_close), ("\\|", _inter_divider)]
+	return _replaceMultiple(text, replacements)
+
+
+def _convertFinalEscapeCodes(text):
+	replacements = [
+		(_inter_open, "["), (_inter_close, "]"), (_inter_divider, "|"),
+		("\\s", " "), ("\\t", "\t"), ("\\n", "\n"), ("\\r", "\r"), ("\\\\", "\\")
+	]
+	return _replaceMultiple(text, replacements)
+
+
+def _tokenize(text):
+	text = _convertIntermediateEscapeCodes(text)
+	return [x for x in splitter.split(text) if x.strip() != ""]
+
+
+def _parseList(tokIter, endAtBar):
+	try:
+		tree, tok = [], tokIter.next()
+
+		while tok != "]":
+			if tok == "[":
+				subTree, tok = _parseList(tokIter, False)
+				tree.append(subTree)
+
+			elif tok == "|":
+				if endAtBar: break
+				tree = [tree]
+				while tok != "]":
+					subTree, tok = _parseList(tokIter, True)
+					tree.append(subTree)
+				break
+
+			else:
+				tree.append(_convertFinalEscapeCodes(tok))
+
+			tok = tokIter.next()
+
+	except StopIteration:
+		return tree, tok
+	else:
+		return tree, tok
+
+
+def _parseRoot(tokIter):
+	try:
+		tok = tokIter.next()
+		if tok != "[": raise RuntimeError("TML: Expected open bracket")
+	except StopIteration: raise RuntimeError("TML: Expected open bracket (empty TML text)")
+
+	tree, _ = _parseList(tokIter, False)
+
+	try: tok = tokIter.next()
+	except StopIteration: pass
+	else: raise RuntimeError("TML: Expected end of file after root node")
+	
+	return tree
+
+
+def _treeToStringGen(tmlTree, brackets):
+	if type(tmlTree) is not list: return tmlTree
+	siblings = [_treeToStringGen(x, brackets) for x in tmlTree]
+	text = reduce((lambda a,b: a+" "+b), siblings)
+	if brackets: return "[" + text + "]"
+	else: return text
+
+
+def parse(tmlString):
+	"""Parses the given TML string and returns the result as a "tree" (nested lists and strings)."""
+	tokens = _tokenize(tmlString)
+	return _parseRoot(tokens.__iter__())
+
+
+def toString(tmlTree):
+	"""Converts a given nested lists/strings "tree" object into valid TML code."""
+	return _treeToStringGen(tmlTree, False)
+
+
+def toMarkupString(tmlTree):
+	"""Converts a given nested lists/strings "tree" object into a string with each item separated by a space."""
+	return _treeToStringGen(tmlTree, True)
+
+
